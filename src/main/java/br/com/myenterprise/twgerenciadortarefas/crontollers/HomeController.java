51970c3b327b95f155a179dc91b61887c5c3ccfc
nicolas-santanna-campos <nicolassant.anna@gmail.com>
2019-09-08 18:44:08 -0300

    Inclusão de retorno do tipo ModelAndView.

--- src/main/java/br/com/myenterprise/twgerenciadortarefas/crontollers/HomeController.java
+++ src/main/java/br/com/myenterprise/twgerenciadortarefas/crontollers/HomeController.java
@@ -2,12 +2,18 @@ package br.com.myenterprise.twgerenciadortarefas.crontollers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
 
 	@GetMapping("/")
-	public String home() {
-		return "home/home";
+	public ModelAndView home() {
+		
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("home/home");
+		modelAndView.addObject("mensagem", "Mensagem do Controller");
+		
+		return modelAndView;
 	}
 }
